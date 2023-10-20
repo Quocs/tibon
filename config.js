21dff05461c4d326f879fb3b0f80c077b0acee33
Quocs <50492579+Quocs@users.noreply.github.com>
2023-10-20 21:03:12 +0700

    Add files via upload
    Create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em Yêu';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Em Yêu 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'tibon.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
